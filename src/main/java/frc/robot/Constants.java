073ac7e4e9e07f0149e1ff8242d01d7aad25ee80
Jake Armstrong <Lainkai@users.noreply.github.com>
2021-01-23 08:38:31 -0500

    Robot Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,13 +20,10 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public class Constants {
-   /**
-    * public static final class DriveConstants {
-    *   public static final int kLeftMotor1Port = 0;
-    *   public static final int kLeftMotor2Port = 1;
-    *   public static final int kRightMotor1Port = 2;
-    *   public static final int kRightMotor2Port = 3; 
-    * }
-    */ 
+
+    public static final class DriveConstants {
+        public static final int kLeft = 0;
+        public static final int kRight = 1;
+      } 
 }
 
