666e7746fa12f08e3275fc3b5593fd9bcfce472e
Jake Armstrong <Lainkai@users.noreply.github.com>
2021-03-01 13:08:50 -0500

    Started Source Video

---
+++ media/classifier data/generate_frames.py
@@ -0,0 +1,28 @@
+#By BeeBee8 on Stackoverflow
+#https://stackoverflow.com/a/47632941
+
+import sys
+import argparse
+
+import cv2
+print(cv2.__version__)
+
+def extractImages(pathIn, pathOut, time_between):
+    count = 0
+    vidcap = cv2.VideoCapture(pathIn)
+    success,image = vidcap.read()
+    success = True
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*time_between))    # added this line 
+        success,image = vidcap.read()
+        print ('Read a new frame: ', success)
+        cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+        count = count + 1
+
+if __name__=="__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("--pathIn", help="path to video")
+    a.add_argument("--pathOut", help="path to images")
+    args = a.parse_args()
+    print(args)
+    extractImages(args.pathIn, args.pathOut,1000/60)
