cf2fc609def4e8473ab8f7353acde555c850103d
Jake Armstrong <Lainkai@users.noreply.github.com>
2021-03-11 11:30:28 -0500

    Wait for deadman

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -129,6 +129,10 @@ public class Robot extends TimedRobot {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
         m_robotContainer.setAutoStatus(20);
 
+        System.out.println("Waiting for deadman.");
+        while(!m_robotContainer.getDeadman()) {}
+        System.out.println("Deadman Ready.");
+
         // schedule the autonomous command (example)
         if (m_autonomousCommand != null) {
             m_autonomousCommand.schedule();
