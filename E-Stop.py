e9f6650d3c32264f8c1381f7cca8b1ed9c7efa92
Jake Armstrong <Lainkai@users.noreply.github.com>
2021-03-10 23:33:19 -0500

    Create E-Stop.py

---
+++ E-Stop.py
@@ -0,0 +1,14 @@
+from networktables import NetworkTables
+import keyboard
+import sys
+
+import logging
+
+#TODO Register to Networktables.
+
+#TODO register keyboard listener to shift
+
+#TODO on release: set safety false
+#TODO on down: set safety true
+
+#TODO if something else sets safety, reset it.
